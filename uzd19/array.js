a4749c7c2b04f463fcbd1c977ef3a04aa4c487ab
Your Name <you@example.com>
2023-12-04 15:01:13 +0200

    2023-12-04 Pirminiai

---
+++ uzd19/array.js
@@ -0,0 +1,130 @@
+const small = [1, 0, 1, 0, 1, 0, 1];
+
+const farm = [
+  { name: "Moo", type: "cow" },
+  { name: "Oink", type: "pig" },
+  { name: "Cluck", type: "chicken" },
+  { name: "Neigh", type: "horse" },
+  { name: "Baa", type: "sheep" },
+  { name: "Meow", type: "cat", color: "black" },
+  { name: "Woof", type: "dog" },
+  { name: "Moo", type: "cow" },
+  { name: "Oink", type: "pig" },
+  { name: "Cluck", type: "chicken" },
+  { name: "Neigh", type: "horse" },
+  { name: "Baa", type: "sheep" },
+  { name: "Meow", type: "cat", color: "white" },
+  { name: "Woof", type: "dog" },
+  { name: "Moo", type: "cow" },
+  { name: "Oink", type: "pig" },
+  { name: "Cluck", type: "chicken" },
+  { name: "Neigh", type: "horse" },
+  { name: "Baa", type: "sheep" },
+  { name: "Meow", type: "cat", color: "brown" },
+  { name: "Woof", type: "dog" },
+  { name: "Moo", type: "cow" },
+  { name: "Oink", type: "pig" },
+  { name: "Cluck", type: "chicken" },
+  { name: "Neigh", type: "horse" },
+  { name: "Baa", type: "sheep" },
+  { name: "Meow", type: "cat", color: "white and black" },
+  { name: "Woof", type: "dog" },
+  { name: "Moo", type: "cow" },
+  { name: "Oink", type: "pig" },
+  { name: "Cluck", type: "chicken" },
+  { name: "Neigh", type: "horse" },
+  { name: "Baa", type: "sheep" },
+  { name: "Meow", type: "cat", color: "tricolor" },
+  { name: "Woof", type: "dog" },
+  { name: "Moo", type: "cow" },
+];
+
+const noPigs = farm.filter((item) => item.type != "pig");
+
+console.log(noPigs);
+
+const catsAndDogs = farm.filter(
+  (item) => item.type == "cat" || item.type == "dog"
+);
+
+console.log(catsAndDogs);
+
+const whiteDogs = farm
+  .filter((item) => item.type == "dog")
+  .map((item) => ({ ...item, color: "white" }));
+
+console.log(whiteDogs);
+
+const findCat = farm.find((item) => item.type == "cat");
+
+const findCatColor = farm.find((item) => item.type == "color")?.color;
+
+console.log(findCat);
+console.log(findCatColor);
+
+const dog = { name: "Woof" };
+
+console.log(dog?.name);
+
+let catCount = 0;
+
+const secondCat = farm.find((item) => item.type == "cat" && ++catCount == 2);
+
+console.log(secondCat);
+
+let catsDogCounter = 0;
+
+// const first5CatsOrDogs = farm.filter(item => (item.type == 'cat' || item.type == 'dog') && ++catsDogCounter <= 5);
+
+const first5CatsOrDogs = farm.filter(
+  item => (item.type == "cat" || item.type == "dog") && ++catsDogCounter <= 4);
+console.log(first5CatsOrDogs);
+
+const animals = [
+  {name: 'Fancy', species: 'dog', age: 5},
+  {name: 'Poncho', species: 'dog', age: 10},
+  {name: 'Tom', species: 'cat', age: 3},
+  {name: 'Jerry', species: 'cat', age: 1},
+  {name: 'Bella', species: 'dog', age: 12},
+  {name: 'Charlie', species: 'dog', age: 8},
+  {name: 'Max', species: 'cat', age: 7}
+];
+
+const ageOfAnimals = animals.reduce((sum, item) => sum + item.age, 0);
+console.log(ageOfAnimals);
+
+const averageAgeOfAnimals = animals.reduce((sum, item, index, array) => {
+  sum += item.age;
+
+  if (index === array.length - 1) {
+    return sum / array.length;
+  }
+
+  return sum;
+}, 0);
+
+console.log(averageAgeOfAnimals);
+
+const maxAgeOfAnimals = animals.reduce((max, item) => max > item.age ? max : item.age, 0);
+
+console.log(maxAgeOfAnimals);
+
+// animals.sort((a, b) => a.age - b.age);
+// animals.sort((a, b) => b.name.localeCompare(a.name));
+animals.sort((a, b) => {
+  if (a.species < b.species) {
+    return -1;
+  }
+  if (a.species > b.species) {
+    return 1;
+  }
+  if (a.age < b.age) {
+    return -1;
+  }
+  if (a.age > b.age) {
+    return 1;
+  }
+  return 0;
+});
+console.log(animals);
+
