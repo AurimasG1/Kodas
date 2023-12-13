0e2893a429c79469d0d0a50ca590ac8f9cdd05cd
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2023-12-13 12:49:16 +0200

    2023-12-13_Veikia

--- a1/src/App.js
+++ a1/src/App.js
@@ -1,13 +1,25 @@
-import Racoon from 'C:/Users/aurim/Documents/GitHub/Kodas/a1/src/Components/025/Racoon.js'
 import './App.css';
+import Racoon from './Components/025/Racoon'
+
 
 function App() {
+
+  const randomColor = _ => '#' + Math.floor(Math.random() * 16777215).toString(16).padEnd(6, '0');
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>025</p>
-        <Racoon></Racoon>
-        <Racoon/>
+        <h1 style={
+          {color: 'crimson',
+            backgroundColor: randomColor(),
+            // backgroundColor: 'black'
+          }
+        }>React 25</h1>
+        <Racoon name={' Jonas'} color={'darkorange'} svoris={25} randomColor={randomColor}>
+          <h4>Inside</h4>
+        </Racoon>
+        <Racoon name={' Ona'} color={'skyblue'} svoris={36} randomColor={randomColor}/>
+        <h4>Inside</h4>
       </header>
     </div>
   );
---
+++ a1/src/Components/025/Rabit.jsx
@@ -0,0 +1,12 @@
+export default function Rabit({randomColor}) {
+
+    return (
+        <h2 style={
+            {
+                background: 'pink',
+                padding: '1em',
+                color: randomColor()
+            }
+        }>raBIT componentas</h2>
+    )
+}
