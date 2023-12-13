7635990e5b178f57bb905f85b73cd8904addd76d
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2023-12-13 10:53:07 +0200

    2023-12-13_Pavyko

---
+++ a1/src/App.js
@@ -0,0 +1,16 @@
+import Racoon from 'C:/Users/aurim/Documents/GitHub/Kodas/a1/src/Components/025/Racoon.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>025</p>
+        <Racoon></Racoon>
+        <Racoon/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ a1/src/Components/025/Racoon.js
@@ -0,0 +1,17 @@
+export default function Racoon() {
+
+    return (
+
+        <>
+        
+        <h2>Racoon</h2>
+        <i>
+            {
+                [1, 2, 3].forEach(item => item)
+            }
+        </i>
+        <div>Nice animal</div>
+        </>
+    );
+
+}
